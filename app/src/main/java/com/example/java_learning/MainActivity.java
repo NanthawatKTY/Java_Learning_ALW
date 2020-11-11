21fd1774d1501234bd33845789bf2b3bb3097ec6
NanthawatKTY <nanthawat.tae123@gmail.com>
2020-11-11 18:38:24 +0700

    Tae 2nd commit: Added characters IMG

--- app/src/main/java/com/example/java_learning/MainActivity.java
+++ app/src/main/java/com/example/java_learning/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.java_learning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView HeaderView;
     private ListView ListViewData;
     private Button btnViewInfo;
+    private String UrlViewInfo = "https://github.com/NanthawatKTY/Practice_Kotlin/tree/master";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +30,26 @@ public class MainActivity extends AppCompatActivity {
         btnViewInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HeaderView.setText("Test clicking");
-
-                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.mouse_click);
-                mediaPlayer.start();
+                TestSetTxt("Pratu");
+                PlayClickViewSound();
+                OpenWeb();
             }
-
         });
+    // Main met created
     }
+
+    private void TestSetTxt(String headerTxt){
+        HeaderView.setText(headerTxt);
+    } //Func SetText
+
+    private void PlayClickViewSound(){
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.mouse_click);
+        mediaPlayer.start();
+    } //Func PlaySound
+
+    private void OpenWeb(){
+        Intent intentToWebInfo = new Intent(Intent.ACTION_VIEW);
+        intentToWebInfo.setData(Uri.parse(UrlViewInfo));        //String URL uses parse()
+        startActivity(intentToWebInfo);
+    } // Func OpenWeb
 }
